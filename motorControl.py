1f7c5ee4bc246984c63e793a86c3dc2072ffa09c
Linard <linard.gauthier@gmail.com>
2015-09-23 15:44:22 +0200

    Ajout d'une boucle

--- motorControl.py
+++ motorControl.py
@@ -12,30 +12,32 @@ Motor2A = 27
 Motor2B = 22
 Motor2E = 17
 
-GPIO.setup(Motor1A, GPIO.OUT)
-GPIO.setup(Motor1B, GPIO.OUT)
-GPIO.setup(Motor1E, GPIO.OUT)
+while true:
+	GPIO.setup(Motor1A, GPIO.OUT)
+	GPIO.setup(Motor1B, GPIO.OUT)
+	GPIO.setup(Motor1E, GPIO.OUT)
 
-GPIO.setup(Motor2A, GPIO.OUT)
-GPIO.setup(Motor2B, GPIO.OUT)
-GPIO.setup(Motor2E, GPIO.OUT)
+	GPIO.setup(Motor2A, GPIO.OUT)
+	GPIO.setup(Motor2B, GPIO.OUT)
+	GPIO.setup(Motor2E, GPIO.OUT)
 
-print "Going forwards"
-GPIO.output(Motor1A, GPIO.HIGH)
-GPIO.output(Motor1B, GPIO.LOW)
-GPIO.output(Motor1E, GPIO.HIGH)
+	print "Going forwards"
+	GPIO.output(Motor1A, GPIO.HIGH)
+	GPIO.output(Motor1B, GPIO.LOW)
+	GPIO.output(Motor1E, GPIO.HIGH)
 
-GPIO.output(Motor2A, GPIO.HIGH)
-GPIO.output(Motor2B, GPIO.LOW)
-GPIO.output(Motor2E, GPIO.HIGH)
+	GPIO.output(Motor2A, GPIO.HIGH)
+	GPIO.output(Motor2B, GPIO.LOW)
+	GPIO.output(Motor2E, GPIO.HIGH)
 
-sleep(2)
+	sleep(10)
 
-print "Going backwards"
-GPIO.output(Motor1A, GPIO.LOW)
-GPIO.output(Motor1B, GPIO.HIGH)
-GPIO.output(Motor1E, GPIO.HIGH)
+	print "Going backwards"
+	GPIO.output(Motor1A, GPIO.LOW)
+	GPIO.output(Motor1B, GPIO.HIGH)
+	GPIO.output(Motor1E, GPIO.HIGH)
 
-GPIO.output(Motor2A, GPIO.LOW)
-GPIO.output(Motor2B, GPIO.HIGH)
-GPIO.output(Motor2E, GPIO.HIGH)
+	GPIO.output(Motor2A, GPIO.LOW)
+	GPIO.output(Motor2B, GPIO.HIGH)
+	GPIO.output(Motor2E, GPIO.HIGH)
+
